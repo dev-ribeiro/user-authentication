86b2a62a3148a3fb798e4b7547e9b706566790fe
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-05-15 17:11:51 -0300

    fix: adjust logout redirect

--- src/views/assets/js/profile.js
+++ src/views/assets/js/profile.js
@@ -2,11 +2,19 @@ const firstNameInput = document.getElementById('firstName');
 const lastNameInput = document.getElementById('lastName');
 const emailText = document.getElementById('email');
 const password = document.getElementById('password');
+const logout = document.getElementById('logout');
 const confirmPassword = document.getElementById('confirmPassword');
-const STORAGE_KEY = "@user-authentication___v1";
+const STORAGE_KEY = '@user-authentication___v1';
+const BASE_PATH = 'http://localhost:3335';
 
 onload = () => {
     setProfilePreviusData();
+
+    logout.addEventListener('click', () => {
+        localStorage.removeItem(STORAGE_KEY);
+
+        window.location.href = BASE_PATH;
+    })
 }
 
 function setProfilePreviusData() {
