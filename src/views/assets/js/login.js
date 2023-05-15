269a42838db9968f0a0005ac861a912e71a68e07
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-05-15 13:48:49 -0300

    feat: create initial views of project

---
+++ src/views/assets/js/login.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const BASE_URL = "http://localhost:3335";
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    validateUserLogin(email.value, password.value);
+});
+
+async function validateUserLogin(email, password) {
+    const response = await fetch(`/api/login/${email}`, {
+        method: 'POST',
+        headers: {
+            password
+        }
+    });
+
+    if(response.status !== 200) {
+        window.location.href = `${BASE_URL}/error`;
+        return;
+    }
+
+    window.location.href = `${BASE_URL}/user`;
+}
