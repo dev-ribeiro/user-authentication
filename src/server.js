989e700d427b7c37e4488dc16f49fc142b1fc604
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-05-18 19:42:45 -0300

    fix: separate app of server file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const { router } = require('./routes/routes')
-
-const server = express();
-
-server.use(express.json());
-server.use(router);
+const { app } = require('./app');
 
 const PORT = process.env.PORT || 3335;
-server.listen(PORT, () => console.log('Server running on port ', PORT));
+app.listen(PORT, () => console.log('Server running on port ', PORT));
